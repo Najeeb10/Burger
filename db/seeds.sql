71f374deba8d0a07706f97b675a1172a1bd2b141
Najeeb Sabour <najeeb@najeebs-mbp.attlocal.net>
2018-02-27 20:42:08 -0600

    files

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Party Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Westcoast Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spinach Artichoke Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black & Yellow Burger');
+
+INSERT INTO menu (item) VALUES ('Party Swiss Burger');
+INSERT INTO menu (item) VALUES ('Westcoast Veggie Burger');
+INSERT INTO menu (item) VALUES ('Spinach Artichoke Beef Burger');
+INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
+INSERT INTO menu (item) VALUES ('Garden & shroom Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Monster Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Curly Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
